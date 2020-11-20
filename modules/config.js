f220726b203b14421e666cb23394ac11d3fa9a41
VK Dolea <66521243+vkdolea@users.noreply.github.com>
2020-11-20 13:54:40 -0300

    Added function to read data from compendium and store name and _id
    Generic function, compendium name as input

--- modules/config.js
+++ modules/config.js
@@ -12,24 +12,31 @@ ageSystem.attributes = {
     will: "age-system.will",
 };
 
-ageSystem.focus = {
-    teste1: "um",
-    teste2: "dois"
-};
+// Initializing variable
+ageSystem.focus = [];
 
-Hooks.on("renderageSystemItemSheet", function() {
-    setTimeout(function () {
-        ageSystem.focus.teste2 = focusList()[0];
-    }, 2000)
+// Hook to update compendium list when Foundry VTT is 'ready'
+Hooks.on("ready", function() {
+    ageSystem.focus = compendiumList("age-system.focus");
 });
 
-function focusList() {
-    let focusPack = game.packs.get("age-system.focus");
-    focusPack.getIndex();
-    let focusList = [];
-    for (let i = 0; i < focusPack.index.length; i++) {
-        const entry = focusPack.index[i];
-        focusList[i] = entry.name;
-    }
-    return focusList;
+// If Compendia are updated, then compendiumList is gathered once again
+Hooks.on("renderCompendium", function() {
+    ageSystem.focus = compendiumList("age-system.focus");
+});
+
+// This function looks at given Compendium and returns an array with object containing id and name for all entries
+function compendiumList(compendiumName) {
+    let dataPack = game.packs.get(compendiumName);
+    let dataList = [];
+    dataPack.getIndex().then(function(){
+        for (let i = 0; i < dataPack.index.length; i++) {
+            const entry = dataPack.index[i]; // It is necessary to store entry's name and id, to avoid messing up with existing Focus when Compendium is updated! Create array of objectes array = [{id: "xxx", name: "yyy"} {...}] - check if my implementation is correct
+            dataList[i] = {
+                _id: entry._id,
+                name: entry.name
+            };   
+        }
+    });
+    return dataList;
 }
